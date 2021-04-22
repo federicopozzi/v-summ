56b2d722d274273060ffde2138b4614896677283
Federico <federico.izzop@gmail.com>
2021-04-22 11:26:14 +0200

    header + home

--- src/components/HeaderHome.js
+++ src/components/HeaderHome.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'gatsby-link';
-import { ReactComponent as ALink } from '../assets/svg/arrowLink.svg';
+import { ReactComponent as ArrowLink } from '../assets/svg/arrowLink.svg';
 
 import '../scss/style.scss';
 
@@ -16,7 +16,7 @@ export default function HeaderHome() {
 
               <Link to="/home" className="linkHead">
                 HOME
-                <ALink style={{ transform: 'scale(0.8)' }} />
+                <ArrowLink style={{ transform: 'scale(0.8)' }} />
               </Link>
 
             </p>
@@ -29,12 +29,12 @@ export default function HeaderHome() {
           <div className="col-3">
             <p className="fs-3">
               ABOUT
-              <ALink style={{ transform: 'scale(0.8)' }} />
+              <ArrowLink style={{ transform: 'scale(0.8)' }} />
             </p>
             <p className="fs-3">
               <Link to="/" className="linkHead">
                 INDEX
-                <ALink style={{ transform: 'scale(0.8)' }} />
+                <ArrowLink style={{ transform: 'scale(0.8)' }} />
               </Link>
             </p>
           </div>
