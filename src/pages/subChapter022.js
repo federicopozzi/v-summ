8c2478cba454fd57aadea03440fd713958988f6d
Federico <federico.izzop@gmail.com>
2021-04-24 21:26:48 +0200

    funziona se rompo torno qui

--- src/pages/subChapter022.js
+++ src/pages/subChapter022.js
@@ -14,6 +14,9 @@ export default function SubChapter02() {
     primo, colore, coloreBordo, rq,
   } = headerPages;
 
+  const methodology = data[3];
+
+  const { aim, output } = methodology;
   return (
     <div>
       <div>
@@ -55,6 +58,33 @@ export default function SubChapter02() {
                           </div>
                         ))
                     }
+        </div>
+      </div>
+      <div className="container-fluid">
+        <div className={`row border-end border-bottom border-top bg-light py-1 ${coloreBordo}`}>
+          <p>
+            METODOLOGY
+            <ArrowD style={{ transform: 'scale(0.6)' }} />
+          </p>
+        </div>
+        <div className="row bg-primary py-4">
+          <div className="col-2">
+            <span className="fs-2 text-decoration-underline">aim</span>
+          </div>
+          <div className="col-10">
+            <p className="fs-2">
+              {aim}
+            </p>
+          </div>
+          <div className="col-2">
+            <span className="fs-2 text-decoration-underline">output</span>
+          </div>
+          <div className="col-10">
+            <p className="fs-2">
+              {' '}
+              {output}
+            </p>
+          </div>
         </div>
       </div>
     </div>
