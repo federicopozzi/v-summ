5bc62469c728b609d276d1e759d9670968999e02
Andrea Elena Febres Medina <andreae.febresm@gmail.com>
2021-04-25 15:48:17 +0200

    prayamazon

--- src/data/subCh021/subChapters021.js
+++ src/data/subCh021/subChapters021.js
@@ -1,5 +1,23 @@
 import prayAmazon1 from '../../images/ch02/subPrayAmazon/1.png';
+import prayAmazon2 from '../../images/ch02/subPrayAmazon/2.png';
+import prayAmazon3 from '../../images/ch02/subPrayAmazon/3.png';
+import prayAmazon4 from '../../images/ch02/subPrayAmazon/4.png';
+import prayAmazon5 from '../../images/ch02/subPrayAmazon/5.png';
+import prayAmazon6 from '../../images/ch02/subPrayAmazon/6.png';
+import prayAmazon7 from '../../images/ch02/subPrayAmazon/7.png';
+import prayAmazon8 from '../../images/ch02/subPrayAmazon/8.png';
+import prayAmazon9 from '../../images/ch02/subPrayAmazon/9.png';
+import prayAmazon10 from '../../images/ch02/subPrayAmazon/10.png';
 import prayLegend1 from '../../images/ch02/subPrayAmazon/prayLegend1.png';
+import prayLegend2 from '../../images/ch02/subPrayAmazon/prayLegend2.png';
+import prayLegend3 from '../../images/ch02/subPrayAmazon/prayLegend3.png';
+import prayLegend4 from '../../images/ch02/subPrayAmazon/prayLegend4.png';
+import prayLegend5 from '../../images/ch02/subPrayAmazon/prayLegend5.png';
+import prayLegend6 from '../../images/ch02/subPrayAmazon/prayLegend6.png';
+import prayLegend7 from '../../images/ch02/subPrayAmazon/prayLegend7.png';
+import prayLegend8 from '../../images/ch02/subPrayAmazon/prayLegend8.png';
+import prayLegend9 from '../../images/ch02/subPrayAmazon/prayLegend9.png';
+import prayLegend10 from '../../images/ch02/subPrayAmazon/prayLegend10.png';
 
 export const subChapter011 = {
   titolo: 'video: Je BALAYE les Bobo PRAY-FOR-AMAZONIA - Expliquez-Moi Cette Merde #13',
@@ -10,63 +28,63 @@ export const subChapter011 = {
 
 export const subChapter021 = {
   titolo: 'AMAZON RAINFOREST ON FIRE LUNGS OF THE WORDL INF FLAME NIGHTTIME',
-  img: prayAmazon1,
-  legend: prayLegend1,
+  img: prayAmazon2,
+  legend: prayLegend2,
 
 };
 
 export const subChapter031 = {
   titolo: 'AMAZON RAINFOREST ON FIRE LUNGS OF THE WORDL INF FLAME NIGHTTIME',
-  img: prayAmazon1,
-  legend: prayLegend1,
+  img: prayAmazon3,
+  legend: prayLegend3,
 
 };
 
 export const subChapter041 = {
   titolo: 'AMAZON RAINFOREST ON FIRE LUNGS OF THE WORDL INF FLAME NIGHTTIME',
-  img: prayAmazon1,
-  legend: prayLegend1,
+  img: prayAmazon4,
+  legend: prayLegend4,
 
 };
 
 export const subChapter051 = {
   titolo: 'AMAZON RAINFOREST ON FIRE LUNGS OF THE WORDL INF FLAME NIGHTTIME',
-  img: prayAmazon1,
-  legend: prayLegend1,
+  img: prayAmazon5,
+  legend: prayLegend5,
 
 };
 
 export const subChapter061 = {
   titolo: 'AMAZON RAINFOREST ON FIRE LUNGS OF THE WORDL INF FLAME NIGHTTIME',
-  img: prayAmazon1,
-  legend: prayLegend1,
+  img: prayAmazon6,
+  legend: prayLegend6,
 
 };
 
 export const subChapter071 = {
   titolo: 'AMAZON RAINFOREST ON FIRE LUNGS OF THE WORDL INF FLAME NIGHTTIME',
-  img: prayAmazon1,
-  legend: prayLegend1,
+  img: prayAmazon7,
+  legend: prayLegend7,
 
 };
 
 export const subChapter081 = {
   titolo: 'AMAZON RAINFOREST ON FIRE LUNGS OF THE WORDL INF FLAME NIGHTTIME',
-  img: prayAmazon1,
-  legend: prayLegend1,
+  img: prayAmazon8,
+  legend: prayLegend8,
 
 };
 
 export const subChapter091 = {
   titolo: 'AMAZON RAINFOREST ON FIRE LUNGS OF THE WORDL INF FLAME NIGHTTIME',
-  img: prayAmazon1,
-  legend: prayLegend1,
+  img: prayAmazon9,
+  legend: prayLegend9,
 
 };
 
 export const subChapter101 = {
   titolo: 'AMAZON RAINFOREST ON FIRE LUNGS OF THE WORDL INF FLAME NIGHTTIME',
-  img: prayAmazon1,
-  legend: prayLegend1,
+  img: prayAmazon10,
+  legend: prayLegend10,
 
 };
