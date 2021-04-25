ffb4a433d016236f2c4662cd0be02967a26ff975
Federico <federico.izzop@gmail.com>
2021-04-25 19:24:00 +0200

    change gif chapter 05

--- src/pages/chapter05.js
+++ src/pages/chapter05.js
@@ -3,12 +3,14 @@ import HeaderPages from '../components/HeaderPages';
 import '../scss/style.scss';
 import Methodology from '../components/Methodology';
 import Ch05Index from '../components/ch05/ch05Index';
+import Ch05Tab from '../components/ch05/ch05Tab';
 
 export default function Chapter05() {
   return (
     <div>
       <HeaderPages />
       <Ch05Index />
+      <Ch05Tab />
       <Methodology />
     </div>
   );
--- src/pages/chapter051.js
+++ src/pages/chapter051.js
@@ -3,12 +3,14 @@ import HeaderPages from '../components/HeaderPages';
 import '../scss/style.scss';
 import Methodology from '../components/Methodology';
 import Ch051Index from '../components/ch05/ch051Index';
+import Ch05Tab from '../components/ch05/ch05Tab';
 
 export default function Chapter051() {
   return (
     <div>
       <HeaderPages />
       <Ch051Index />
+      <Ch05Tab />
       <Methodology />
     </div>
   );
