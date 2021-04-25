ded615b5c89e4760bb5c30a477e6d9d194af2eec
Andrea Elena Febres Medina <andreae.febresm@gmail.com>
2021-04-25 15:37:45 +0200

    amazonfires

--- src/data/subCh02/subChapters02.js
+++ src/data/subCh02/subChapters02.js
@@ -1,7 +1,23 @@
 import amazonFires1 from '../../images/ch02/subAmazonFires/1.png';
 import amazonFires2 from '../../images/ch02/subAmazonFires/2.png';
+import amazonFires3 from '../../images/ch02/subAmazonFires/3.png';
+import amazonFires4 from '../../images/ch02/subAmazonFires/4.png';
+import amazonFires5 from '../../images/ch02/subAmazonFires/5.png';
+import amazonFires6 from '../../images/ch02/subAmazonFires/6.png';
+import amazonFires7 from '../../images/ch02/subAmazonFires/7.png';
+import amazonFires8 from '../../images/ch02/subAmazonFires/8.png';
+import amazonFires9 from '../../images/ch02/subAmazonFires/9.png';
+import amazonFires10 from '../../images/ch02/subAmazonFires/10.png';
 import legendAmazonFires1 from '../../images/ch02/subAmazonFires/firesLegend1.png';
 import legendAmazonFires2 from '../../images/ch02/subAmazonFires/firesLegend2.png';
+import legendAmazonFires3 from '../../images/ch02/subAmazonFires/firesLegend3.png';
+import legendAmazonFires4 from '../../images/ch02/subAmazonFires/firesLegend4.png';
+import legendAmazonFires5 from '../../images/ch02/subAmazonFires/firesLegend5.png';
+import legendAmazonFires6 from '../../images/ch02/subAmazonFires/firesLegend6.png';
+import legendAmazonFires7 from '../../images/ch02/subAmazonFires/firesLegend7.png';
+import legendAmazonFires8 from '../../images/ch02/subAmazonFires/firesLegend8.png';
+import legendAmazonFires9 from '../../images/ch02/subAmazonFires/firesLegend9.png';
+import legendAmazonFires10 from '../../images/ch02/subAmazonFires/firesLegend10.png';
 
 export const subChapter01 = {
   titolo: 'VIDEO: ENTENDA O DESMANTAMENTO NA AMAZÔNIA DE UM JEITO SIMPLES',
@@ -19,56 +35,56 @@ export const subChapter02 = {
 
 export const subChapter03 = {
   titolo: 'AMAZON RAINFOREST ON FIRE LUNGS OF THE WORDL INF FLAME NIGHTTIME',
-  img: amazonFires2,
-  legend: legendAmazonFires2,
+  img: amazonFires3,
+  legend: legendAmazonFires3,
 
 };
 
 export const subChapter04 = {
   titolo: 'AMAZON RAINFOREST ON FIRE LUNGS OF THE WORDL INF FLAME NIGHTTIME',
-  img: amazonFires2,
-  legend: legendAmazonFires2,
+  img: amazonFires4,
+  legend: legendAmazonFires4,
 
 };
 
 export const subChapter05 = {
   titolo: 'AMAZON RAINFOREST ON FIRE LUNGS OF THE WORDL INF FLAME NIGHTTIME',
-  img: amazonFires2,
-  legend: legendAmazonFires2,
+  img: amazonFires5,
+  legend: legendAmazonFires5,
 
 };
 
 export const subChapter06 = {
   titolo: 'AMAZON RAINFOREST ON FIRE LUNGS OF THE WORDL INF FLAME NIGHTTIME',
-  img: amazonFires2,
-  legend: legendAmazonFires2,
+  img: amazonFires6,
+  legend: legendAmazonFires6,
 
 };
 
 export const subChapter07 = {
   titolo: 'AMAZON RAINFOREST ON FIRE LUNGS OF THE WORDL INF FLAME NIGHTTIME',
-  img: amazonFires2,
-  legend: legendAmazonFires2,
+  img: amazonFires7,
+  legend: legendAmazonFires7,
 
 };
 
 export const subChapter08 = {
   titolo: 'AMAZON RAINFOREST ON FIRE LUNGS OF THE WORDL INF FLAME NIGHTTIME',
-  img: amazonFires2,
-  legend: legendAmazonFires2,
+  img: amazonFires8,
+  legend: legendAmazonFires8,
 
 };
 
 export const subChapter09 = {
   titolo: 'AMAZON RAINFOREST ON FIRE LUNGS OF THE WORDL INF FLAME NIGHTTIME',
-  img: amazonFires2,
-  legend: legendAmazonFires2,
+  img: amazonFires9,
+  legend: legendAmazonFires9,
 
 };
 
 export const subChapter10 = {
   titolo: 'AMAZON RAINFOREST ON FIRE LUNGS OF THE WORDL INF FLAME NIGHTTIME',
-  img: amazonFires2,
-  legend: legendAmazonFires2,
+  img: amazonFires10,
+  legend: legendAmazonFires10,
 
 };
