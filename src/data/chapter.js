54419b369937ae111547556b3ee71c201ea756cd
Andrea Elena Febres Medina <andreae.febresm@gmail.com>
2021-04-25 11:56:42 +0200

    fino a 4 rq aim output

--- src/data/chapter.js
+++ src/data/chapter.js
@@ -26,25 +26,18 @@ export const capitolo2 = {
 export const capitolo3 = {
   primo: 'Detecting and visualising the amount of human figures within one or more collections of videos and recognise the different formats.',
   rq: 'RQ: How relevant is the presence of human figures in the Amazon Fires related YouTube videos?',
-  aim: 'This type of analysis can help to recognise different formats where human figures are particularly relevant, such as interviews or news anchors. Applying this method to two different video collections helps us to compare them \n'
-      + 'in terms of the presence of human figures and types of formats. The model used for the extraction of human figures from each frame is DeepLabv3. \n'
-      + 'To perform this type of analysis it is necessary to extract the frames at regular time intervals so as to also record the length of the scenes in which \n'
-      + 'human figures are present and preserve the original length of each video \n'
-      + 'in the final visualisation. \n',
+  aim: 'This type of analysis can help to recognise different formats where human figures are particularly relevant, such as interviews or news anchors. Applying this method to two different video collections helps us to compare them in terms of the presence of human figures and types of formats. The model used for the extraction of human figures from each frame is DeepLabv3. To perform this type of analysis it is necessary to extract the frames at regular time intervals so as to also record the length of the scenes in which human figures are present and preserve the original length of each video in the final visualisation.',
   colore: `rosa ${sfondo}`,
   coloreBordo: 'border-info',
   output: 'The final visualisation is made up of the 20 frame grids of the videos, divided by collection, and allows a direct comparison in terms of the presence of human figures and video formats (news anchors, interviews, image montage). it is possible to explore the videos more closely by selecting one collection.',
 };
 
 export const capitolo4 = {
-  primo: 'capitolo4 of videos.',
-  rq: 'RQ: Which are the main themes dvsved the Amazon Fires related YouTube videos?',
+  primo: 'Recognising the main humans faces in a collection of videos and visually quantifying  their presence.',
+  rq: 'RQ: Which are the most recognisable faces in the Amazon Fires related Youtube videos?',
   viz: ch04,
   colore: `giallo ${sfondo}`,
   coloreBordo: 'border-warning',
-  aim: 'This type of analysis allows us to recognise the faces of human figures and to recognise those most present within a collection of videos. The segmentation model used on RunwayML removes the background from images featuring people. Frame extraction for this purpose is based on scene change detection, so that the images to be analysed are only taken once and there \n'
-      + 'are no duplicates due to scene length.',
-  output: 'The final visualisation is a catalogue of faces of human figures, resized \n'
-      + 'by the amount of presence in the analysed videos, and clustered by type \n'
-      + '(e.g. politicians, bloggers, natives).\n',
+  aim: 'This type of analysis allows us to recognise the faces of human figures and to recognise those most present within a collection of videos. The segmentation model used on RunwayML removes the background from images featuring people. Frame extraction for this purpose is based on scene change detection, so that the images to be analysed are only taken once and there are no duplicates due to scene length.',
+  output: 'The final visualisation is a catalogue of faces of human figures, resized by the amount of presence in the analysed videos, and clustered by type (e.g. politicians, bloggers, natives).',
 };
