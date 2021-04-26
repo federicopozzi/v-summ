6113c882a20ebfb55858189ae0ffd7de2de904fa
Federico <federico.izzop@gmail.com>
2021-04-26 14:47:56 +0200

    subCh051 + tutto a posto qui

--- src/data/subCh05/index.js
+++ src/data/subCh05/index.js
@@ -1,6 +1,17 @@
-import { ch05Viz1, ch05Viz2 } from './subCh05Img';
+import {
+  ch05Viz1, ch05Viz10, ch05Viz2, ch05Viz3, ch05Viz4, ch05Viz5, ch05Viz6, ch05Viz7, ch05Viz8, ch05Viz9,
+} from './subCh05Img';
 
 export default [
   ch05Viz1,
   ch05Viz2,
+  ch05Viz3,
+  ch05Viz4,
+  ch05Viz5,
+  ch05Viz6,
+  ch05Viz7,
+  ch05Viz8,
+  ch05Viz9,
+  ch05Viz10,
+
 ];
