6536468da83c10f3b7723f5d957c4988d4dadebf
Federico <federico.izzop@gmail.com>
2021-04-26 14:07:50 +0200

    subCh051 + tutto a posto qui

---
+++ src/data/subCh051/index.js
@@ -0,0 +1,6 @@
+import { ch05Viz1, ch05Viz2 } from './subCh051Img';
+
+export default [
+  ch05Viz1,
+  ch05Viz2,
+];
---
+++ src/pages/subChapter051.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import HeaderPages from '../components/HeaderPages';
+import '../scss/style.scss';
+import Methodology from '../components/Methodology';
+import Ch05Index from '../components/ch05/ch05Index';
+import Ch05Tab from '../components/ch05/ch05Tab';
+import data from '../data';
+import Link from 'gatsby-link';
+import { ReactComponent as X } from '../assets/svg/x.svg';
+import { ReactComponent as ArrowD } from '../assets/svg/arrowD.svg';
+import SubCh051Viz from '../components/ch05/subCh051Viz';
+
+export default function SubChapter05() {
+  const headerPages = data[6];
+
+  const {
+    primo, colore, coloreBordo, rq,
+  } = headerPages;
+
+  const methodology = data[6];
+
+  const { aim, output } = methodology;
+  return (
+    <div>
+      <div className="container-fluid bg-primary">
+        <div className={`row border-bottom border-1 ${coloreBordo}`}>
+          <div className={`col-11 fs-5 border-end border-2 ${coloreBordo}`}>
+            <div className={`row border-bottom py-3 px-3 border-2 ${coloreBordo}`}>
+              <p>{primo}</p>
+            </div>
+            <div className={colore}>
+              <p className="fw-bold">{rq}</p>
+            </div>
+          </div>
+          <div className="col-1">
+            <Link to="/home"><X style={{ transform: 'scale(0.7)' }} /></Link>
+          </div>
+        </div>
+      </div>
+      <div className={`row border-bottom border-2 bg-light px-3 py-1 ${coloreBordo}`}>
+        <p>
+          VISUALIZATION
+          <ArrowD style={{ transform: 'scale(0.6)' }} />
+
+        </p>
+      </div>
+      <SubCh051Viz />
+      <Ch05Tab />
+      <div className="container-fluid">
+        <div className={`row border-end border-bottom border-top bg-light py-1 ${coloreBordo}`}>
+          <p>
+            METODOLOGY
+            <ArrowD style={{ transform: 'scale(0.6)' }} />
+          </p>
+        </div>
+        <div className="row bg-primary py-4">
+          <div className="col-2">
+            <span className="fs-2 text-decoration-underline">aim</span>
+          </div>
+          <div className="col-10">
+            <p className="fs-2">
+              {aim}
+            </p>
+          </div>
+          <div className="col-2 pt-5">
+            <span className="fs-2 text-decoration-underline">output</span>
+          </div>
+          <div className="col-10 pt-5">
+            <p className="fs-2">
+              {' '}
+              {output}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
