1aa3dbb111b36cfcebbab69f9aee9b812283a1f9
Federico <federico.izzop@gmail.com>
2021-04-26 21:40:23 +0200

    ch06 video

--- src/components/ch06/ch06Viz.js
+++ src/components/ch06/ch06Viz.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { ReactComponent as Title1 } from '../../images/ch06/title1.svg';
+import { ReactComponent as Title2 } from '../../images/ch06/title2.svg';
+
 import a from '../../images/ch06/a.png';
 import b from '../../images/ch06/b.png';
 import c from '../../images/ch06/c.png';
@@ -7,6 +10,8 @@ import d from '../../images/ch06/d.png';
 import '../../scss/style.scss';
 import a1 from '../../images/ch06/a.mp4';
 import a2 from '../../images/ch06/b.mp4';
+import a3 from '../../images/ch06/c.mp4';
+import a4 from '../../images/ch06/d.mp4';
 
 import Link from 'gatsby-link';
 
@@ -19,32 +24,40 @@ export default function Ch06Viz() {
   }
 
   const video1 = {
-    titolo: 'titolo',
+    titolo: '01 - MONTAGE AMAZON FIRES',
     src: a1,
-    set: ' set video',
-    duration: '01',
-    videoOp: '50',
+    set: '01   ENTENDA O DESMAMENTO  |    02   AMAZON RAINFOREST ON FIRE |    03   AMAZON FOREST ODHYSCIENZE\n'
+        + '\n'
+        + '|  04   AMAZON FOREST FIRE WHAT IT TELL ABOUT DEFORESTATION   |  05  DRONE FOOTAGE REVEALS AFTER MATH OF AMAZON FIRES',
+    duration: '00:03:27',
+    videoOp: '40%',
   };
   const video2 = {
-    titolo: 'titolo2',
+    titolo: '02 - GRID AMAZON FIRES',
     src: a2,
-    set: ' set video2',
-    duration: '012',
-    videoOp: '502',
+    set: ' 01   ENTENDA O DESMAMENTO |  02   AMAZON RAINFOREST ON FIRE  |  03   AMAZON FOREST ODHYSCIENZE | 04   AMAZON FOREST FIRE WHAT IT TELL ABOUT DEFORESTATION   | 05  DRONE FOOTAGE REVEALS AFTER MATH OF AMAZON FIRES\n'
+        + '\n'
+        + '| 06   INDIASECRETAMAZONFIRETAMIL  |  07   AMAZONFORESTFIREODHIGYANSCIENCE  |  08  WHAT IT TELLS US ABOUTDEFORESTATION',
+    duration: '00:05:00',
+    videoOp: '100%',
   };
   const video3 = {
-    titolo: 'titolo3',
-    src: a2,
-    set: ' set video3',
-    duration: '012',
-    videoOp: '502',
+    titolo: '03 - MONTAGE PRAY FOR AMAZONIA',
+    src: a3,
+    set: '01   AMAZON FIRE VS METHANE GAS  |   02   PRAY FOR AMAZONIA (SLAM) - SIBYLLIN  |  03   PRAY FOR AMAZONIA \n'
+        + '\n'
+        + '| 04   AMAZON MUSICA DA FLORESTA  |  05   VOCE PRECISA ASSISTEIR ESSE VIDEO  #PRAYFORAMAZONIA',
+    duration: '00:01:37',
+    videoOp: '40%',
   };
   const video4 = {
-    titolo: 'titolo4',
-    src: a2,
-    set: ' set video4',
-    duration: '012',
-    videoOp: '502',
+    titolo: '04 - GRID PRAY FOR AMAZONIA',
+    src: a4,
+    set: ' 01   EJE BALAYE LES BOBO   02   AMAZON FIRE VS METHAN GAS     03   #AMAZONIA #PRAYFORAMAZIONA 04  AMAZON MUSICA DA FLORESTA    05  LA DEVASTADORA RAZON QUE UNE A CRISTIANO RONALDO Y LUIS SUOAREZ\n'
+        + '\n'
+        + '06  PRAY FOR AMAZONIA    07   MACRON URGES ACTION FROM G7 LEADERS  08   FIREFIGHTERS TACKLE FIRES CONTINUING TO BURN',
+    duration: '00:03:31',
+    videoOp: '100%',
   };
 
   const vid = [
@@ -65,7 +78,7 @@ export default function Ch06Viz() {
       <div className="row border-bottom border-white">
         <div className="col-5">
           <div className="row border-bottom border-white">
-            <div className="col-2 border-end border-white">AMAZON FIRE</div>
+            <div className="col-2 border-end border-white d-flex justify-content-center align-items-center"><Title1 /></div>
             <div className="col-5 border-end border-white px-3 py-3">
               {' '}
               <Link to="/chapter06?selected=7&vid=0" className="linkHead">
@@ -94,7 +107,7 @@ export default function Ch06Viz() {
             <div className="col-5 border-end border-white py-2 text-center">GRID</div>
           </div>
           <div className="row border-bottom border-white">
-            <div className="col-2 border-end border-white">PRAY FOR AMAZONIA</div>
+            <div className="col-2 border-end border-white d-flex justify-content-center align-items-center"><Title2 /></div>
             <div className="col-5 border-end border-white px-3 py-3">
               <Link to="/chapter06?selected=7&vid=2" className="linkHead">
                 <img
@@ -120,7 +133,7 @@ export default function Ch06Viz() {
         </div>
         <div className="col-7">
 
-          <div className="row border-bottom border-white">{titolo}</div>
+          <div className="row border-bottom border-white px-3 py-3">{titolo}</div>
           <div className="row">
             <video
               autoPlay
@@ -134,12 +147,12 @@ export default function Ch06Viz() {
 
         </div>
       </div>
-      <div className="row">
-        <div className="col-9 border-end border-white">
+      <div className="row ">
+        <div className="col-9 border-end border-white py-3">
           <p className="fw-bold">SET VIDEOS:</p>
           <p>{set}</p>
         </div>
-        <div className="col-3">
+        <div className="col-3 py-3">
           <div className="row">
             <div className="col-6">
               <p className="fw-bold">DURATION:</p>
